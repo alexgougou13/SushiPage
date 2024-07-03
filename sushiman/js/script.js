2f6f02d839e682249d6cf090ef6c246a868c83fb
alexgougou13 <92690251+alexgougou13@users.noreply.github.com>
2024-07-04 01:38:40 +0300

    Add files via upload

---
+++ sushiman/js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const mobileList = document.querySelector('.mobile-list');
+  const menuIcon = document.getElementById('menu-icon');
+
+  menuToggle.addEventListener('click', function() {
+      if (mobileList.classList.contains('hide')) {
+          mobileList.classList.remove('hide');
+          menuIcon.src = 'assets/close.svg'; 
+      } else {
+          mobileList.classList.add('hide');
+          menuIcon.src = 'assets/menu.svg'; 
+      }
+  });
+
+  document.querySelectorAll('.mobile-list a').forEach(link => {
+      link.addEventListener('click', () => {
+          mobileList.classList.add('hide');
+          menuIcon.src = 'assets/menu.svg'; 
+      });
+  });
+});
